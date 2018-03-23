9030998fa6153e84f7803c0334c6eb74b94eb54f
technicolor1 <gunsofwar999@gmail.com>
2018-03-22 21:27:06 -0400

    -search function
    -some cleanup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,47 @@
 const locater = document.querySelector("button[name=locater]");
+const submit_loc = document.querySelector("button[name=submit-location]");
+
+submit_loc.addEventListener("click", () => {
+   let value = document.querySelector("input[name=field]").value;
+   let google = `https://maps.googleapis.com/maps/api/geocode/json?address=${value}&key=${keys.google}`;
+   let temp = document.querySelector("#temp");
+   let location = document.querySelector("#location");
+   
+   fetch(google)
+      .then(response => {
+         if (response.ok) {
+            return response.json();
+         }
+      })
+      .then(data => {
+         if (data.status === "ZERO_RESULTS") {
+            console.log("Cannot find location. Try a different query");
+            return;
+         }
+         let place = data.results["0"].formatted_address;
+
+         let latitude = data.results["0"].geometry.location.lat;
+         let longitude = data.results["0"].geometry.location.lng;
+         let darksky = `https://api.darksky.net/forecast/${keys.darksky}/${latitude},${longitude}`;
+
+         fetch(darksky)
+            .then(response => {
+               if (response.ok) {
+                  return response.json();
+               }
+            })
+            .then(data => {
+               console.log(data);
+               location.innerHTML = `Weather for ${place}`;
+               temp.innerHTML = `Summary: ${data.daily.summary}<br>
+               Feels like: ${Math.round(data.currently.apparentTemperature)}°F<br>
+               Precipitation: ${data.currently.precipProbability * 100}%<br>
+               Wind: ${data.currently.windSpeed} mph`;
+            })
+      })
+
+   temp.innerHTML = "Obtaining location and weather data...";
+})
 
 locater.addEventListener("click", () => {
    let temp = document.querySelector("#temp");
@@ -25,8 +68,7 @@ locater.addEventListener("click", () => {
          .then(data => {
             console.log(data);
 
-            location.innerHTML = `Weather for ${data.results[2].address_components["0"].long_name}
-            , ${data.results[2].address_components[2].short_name}`
+            location.innerHTML = `Weather for ${data.formatted_address}`;
          })
 
       // Darksky 
@@ -49,36 +91,38 @@ locater.addEventListener("click", () => {
 
       function handleData(data) {
          temp.innerHTML = `Summary: ${data.daily.summary}<br>
-         Feels like: ${Math.round(data.currently.apparentTemperature)}<br>
-         Precipitation: ${data.currently.precipProbability}<br>
-         Wind: ${data.currently.windSpeed}`;
+         Feels like: ${Math.round(data.currently.apparentTemperature)}°F<br>
+         Precipitation: ${data.currently.precipProbability * 100}%<br>
+         Wind: ${data.currently.windSpeed} mph`;
       }
    }
    function fail(error) {
-      temp.innerHTML = "Something went wrong!";
+      let mainmsg = "Something went wrong!<br>";
 
       switch (error.code) {
          case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
+            // console.log("User denied the request for Geolocation.");
+            temp.innerHTML = `${mainmsg}You have denied permissions for Location`;
             break;
          case error.POSITION_UNAVAILABLE:
             console.log("Location information is unavailable.");
+
+            temp.innerHTML = `${mainmsg}Location unavailable`;            
             break;
          case error.TIMEOUT:
             console.log("The request to get user location timed out.");
+            temp.innerHTML = `${mainmsg}Timed out`;
             break;
          case error.UNKNOWN_ERROR:
             console.log("An unknown error occurred.");
             break;
       }
-
-      console.log("Error");
    }
 
    let options = {
       timeout: 10000
    }
 
-   temp.innerHTML = "Obtaining location...";
+   temp.innerHTML = "Obtaining location and weather data...";
    navigator.geolocation.getCurrentPosition(success, fail, options);
 })
